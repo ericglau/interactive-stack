6f90c513a28d7c1d38c193d19fd59575eb90cba0
Eric Lau <ericglau@ca.ibm.com>
2019-11-19 16:38:38 -0500

    Try using console

--- templates/hello/src/main/java/com/sample/App.java
+++ templates/hello/src/main/java/com/sample/App.java
@@ -1,19 +1,16 @@
 package com.sample;
 
-import java.util.Scanner;
-
 public class App {
         public static void main(String[] args) throws Exception {
-                Scanner scanner = new Scanner(System.in);
-                try {
-                        System.out.println("Waiting for input...");
-                        if (scanner.hasNextLine()) {
+                if (System.console() == null) {
+                        System.out.println("Non interactive mode");
+                } else {
+                        System.out.println("Waiting for input...");                        
+                        if (System.console().readLine() != null) {
                                 System.out.println("Line detected");
                         } else {
                                 System.out.println("Non interactive mode");
                         }
-                } finally {
-                        scanner.close();
                 }
         }
 
